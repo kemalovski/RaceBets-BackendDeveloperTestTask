62e92763bc9d38e526332d8c877cd10d8554fdd5
kemalovski <kemalkaraduman36@gmail.com>
2022-02-01 21:45:32 +0100

    witdraw post event bug & everythird bonus bug has been fixed

--- app/app/Http/Services/Transactions/DepositService.php
+++ app/app/Http/Services/Transactions/DepositService.php
@@ -8,18 +8,20 @@ trait DepositService
 {
 
     public function setBonusAmount(){
+        
         $countOfDeposit = DB::table("transactions")
             ->selectRaw('count(id) as countOfDeposit')
             ->where('customer_id', $this->request['customer_id'])
             ->groupBy('customer_id')->first();
-
+            
         $bonusValue = DB::table("bonuses")
             ->selectRaw('bonus_value')
             ->where('customer_id', $this->request['customer_id'])->first();
-        
-        if ($countOfDeposit->countOfDeposit % 3 == 0) {
+            
+        if (isset($countOfDeposit->countOfDeposit) && $countOfDeposit->countOfDeposit % 3 == 2) {
             $this->bonusAmount = ($bonusValue->bonus_value * $this->request['real_amount']) / 100;
         }
+
         return true;
         
     }
